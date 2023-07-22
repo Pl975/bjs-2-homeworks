43c5716ac9cbee0678849ce8dcc9af1bf688651a
pavel shuvalov <pmshuv975@gmail.com>
2023-07-23 01:17:40 +0300

    Fixed rounding

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -89,7 +89,7 @@ function averageEvenElementsWorker(...arr) {
 		}
 	}
 	if (countEvenElement === 0) return 0;
-	return (sumEvenElement / countEvenElement).toFixed(2);
+	return (sumEvenElement / countEvenElement);
 }
 
 // summElementsWorker
